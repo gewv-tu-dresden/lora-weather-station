f029245073d2c5e8ecfc867965d88489166a3794
Bra1nsen <58213050+Bra1nsen@users.noreply.github.com>
2020-01-24 09:25:29 +0100

    Update main.py

--- co2_sensor/main.py
+++ co2_sensor/main.py
@@ -1,29 +1,73 @@
-from scd30.scd30_driver import Pid, SCD30
-from gps.gps_serial import GPS
+from pyb import LED
+import sys, os, pyb
+import machine
+from machine import Pin
 import time
+import utime
+from scd30 import SCD30
+from machine import I2C
 
-p = Pid()
-p.connect()
-pid = p.get_pid()
-scd30 = SCD30(pid)
+errled = LED(1)
+led = LED(2)
 
-gps = GPS()
+# Get Sensor, should be scannable here...
+i2c = I2C(1)
+scd30 = SCD30(i2c, 0x61)
 
-# Set the measurement interval to 2s
-if scd30.read_meas_interval() != 2:
-    print("set interval to 2")
-    scd30.set_interval(2)
+# Make sure we have a SD card!
+sd = pyb.SDCard()
 
-result = [None]
+if sd.present() != 1:
+    errled.on()
+    machine.deepsleep()
+
+# Power off the board...
+sd.power(0)
+
+en_3v3 = Pin('EN_3V3')
+en_3v3.value(0)
+
+
 while True:
-    gps.read_from_port(result)
-    if scd30.is_ready() and result[0]:
-        temp, humidity, co2 = scd30.get_readings()
-        lat, lon = result[0]
-        json = """[{"topic":"envirotron-pi","payload":[{"measurement":"device_frmpayload_data_co2","tags":{"device_name":"rpi"},"fields":{"value":%d}},{"measurement":"device_frmpayload_data_temperature","tags":{"device_name":"rpi"},"fields":{"value":%f}},{"measurement":"device_frmpayload_data_humidity","tags":{"device_name":"rpi"},"fields":{"value":%f}},{"measurement":"device_frmpayload_data_gps_location","tags":{"device_name":"rpi"},"fields":{"latitude":%f,"longitude":%f}}]}]""" % (co2, temp, humidity, lat, lon)
-        print(json)
-        with open('/home/pi/envirotron-pi/readings.json', 'w') as results:
-            results.write(json)
-        result = [None]
-    else:
-        time.sleep(0.1)
+    data = { }
+    en_3v3.value(1)
+    led.on()
+
+    sd.power(1)
+    os.mount(pyb.SDCard(), '/sd')
+
+    # Get local time when taking measurement
+    for i in range(30):
+        t = utime.localtime()
+
+        while scd30.get_status_ready()[0] != 1:
+            time.sleep_ms(200)
+        time.sleep_ms(200)
+        try:
+            measurement = scd30.read_measurement()
+            data['co2'] = measurement[0][0]
+            data['temperature'] = measurement[1][0]
+            data['humidity'] = measurement[2][0]
+            print('co2: {}, temperature: {}, humidity: {}'.format(data['co2'], data['temperature'],
+                data['humidity']))
+        except SCD30.CRCException:
+            print('CRC exception during measurement reading SCD30')
+            errled.on()
+            pass
+
+        # Create logfile per day...
+        datetimeformat = '{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}'
+        isodatetime = datetimeformat.format(t[0], t[1], t[2], t[3], t[4], t[5])
+        name = 'sensordata_{:04d}_{:02d}_{:02d}.log'.format(t[0], t[1], t[2])
+
+        with open("/sd/" + name, "a") as f:
+            f.write("{};{};{};{};\n".format(isodatetime, data['co2'], data['temperature'], data['humidity']))
+
+
+    os.umount('/sd')
+    sd.power(0)
+    en_3v3.value(0)
+    led.off()
+
+    # Sleep for 300 seconds...
+    time.sleep(300)
